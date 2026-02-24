715d29406e958ae7484f5f621ad0f5871ba6d11c
anagha-mangle <anaghamangle@gmail.com>
2026-02-24 17:45:50 +0530

    [feature]: script file

---
+++ script.js
@@ -0,0 +1,13 @@
+const button = document.getElementById("btn");
+
+
+let count = 0;
+
+button.addEventListener("click", function () {
+
+  // Change background to fixed color
+  document.body.style.backgroundColor = "lightblue";
+
+
+
+});
